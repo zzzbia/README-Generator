938b0b955b1eb6763a996602b3b5b1479e328c4c
R a b i a <rabia.saeed.1995@gmail.com>
2022-06-09 17:18:29 -0400

    fixing format of generate markdown and testing

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -32,56 +32,53 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-	if (license === "MIT") {
-		return "MIT License";
-	}
-	if (license === "APACHE") {
-		return "Apache License 2.0";
-	}
-	if (license === "BSD") {
-		return "BSD License";
-	}
 	if (license === "None") {
 		return "";
 	}
+
+	return `
+## 📚 License
+Licensed under the ${renderLicenseLink(license)}.
+`;
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-	return `# ${data.title}
-	
-	## ✏️ Description
-	${data.description}
+	return `
+# ${data.title}
+${renderLicenseBadge(data.license)}
 
-	## 📜 Table of Contents
-	* [Installation](#installation)
-	* [Usage](#usage)
-	* [License](#license)
-	* [Contributing](#contributing)
-	* [Tests](#tests)
-	* [Questions](#questions)
+## ✏️ Description
+${data.description}
 
-	## 💾 Installation
-	${data.installation}
+## 📜 Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+- [License](#license)
 
-	## 💡 Usage
-	${data.usage}
+## 💾 Installation
+${data.installation}
 
-	## License
-	${renderLicenseSection(data.license)}
-	${renderLicenseBadge(data.license)}
-	${renderLicenseLink(data.license)}
+## 💡 Usage
+${data.usage}
 
-	## 👥 Contributing
-	${data.contributing}
 
-	## 🔍 Tests
-	${data.tests}
+## 👥 Contributing
+${data.contributing}
 
-	## 💭 Questions
-	If you have any questions, please contact me at ${
+## 🔍 Tests
+${data.tests}
+
+## 💭 Questions
+If you have any questions, please contact me at ${
 		data.email
 	} or visit my GitHub profile at [github](https://github.com/${data.github})
+
+
+${renderLicenseSection(data.license)}
 
 `;
 }
